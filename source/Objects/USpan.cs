9def5884cf079373fecec058bb86d437324e225b
Phill <philldamaskin@gmail.com>
2024-08-28 13:40:07 -0400

    wip `uint` span

---
+++ source/Objects/USpan.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Unmanaged
+{
+    public readonly unsafe ref struct USpan<T> where T : unmanaged
+    {
+        public readonly void* pointer;
+        public readonly uint length;
+
+        public ref T this[uint index]
+        {
+            get
+            {
+                ThrowIfAccessingOutOfRange(index, length);
+                return ref Unsafe.AsRef<T>((void*)((nint)pointer + index * sizeof(T)));
+            }
+        }
+
+        public USpan<T> this[Range range]
+        {
+            get
+            {
+                uint start = (uint)range.Start.Value;
+                if (range.Start.IsFromEnd)
+                {
+                    start += this.length + 1;
+                }
+
+                uint end = (uint)range.End.Value;
+                if (range.End.IsFromEnd)
+                {
+                    end += this.length + 1;
+                }
+
+                uint length = end - start;
+                return Slice(start, length);
+            }
+        }
+
+        public USpan(void* pointer, uint length)
+        {
+            this.pointer = pointer;
+            this.length = length;
+        }
+
+        public USpan(nint address, uint length)
+        {
+            pointer = (void*)address;
+            this.length = length;
+        }
+
+        public USpan(Span<T> span)
+        {
+            ThrowIfLessThanZero(span.Length);
+            fixed (T* ptr = span)
+            {
+                pointer = ptr;
+                length = (uint)span.Length;
+            }
+        }
+
+        [Conditional("DEBUG")]
+        private static void ThrowIfAccessingOutOfRange(uint index, uint length)
+        {
+            if (index >= length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index must be less than the length of the span.");
+            }
+        }
+
+        [Conditional("DEBUG")]
+        private static void ThrowIfLessThanZero(int length)
+        {
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "Length must be greater than or equal to zero.");
+            }
+        }
+
+        public readonly USpan<T> Slice(uint start, uint length)
+        {
+            ThrowIfAccessingOutOfRange(start, this.length);
+            ThrowIfAccessingOutOfRange(start + length, this.length);
+            return new USpan<T>((void*)((nint)pointer + start * sizeof(T)), length);
+        }
+
+        public readonly T[] ToArray()
+        {
+            T[] array = new T[length];
+            for (uint i = 0; i < length; i++)
+            {
+                array[i] = this[i];
+            }
+
+            return array;
+        }
+
+        public static implicit operator USpan<T>(Span<T> span)
+        {
+            return new(span);
+        }
+    }
+}
---
+++ tests/SpanTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Unmanaged;
+
+namespace Tests
+{
+    public class SpanTests
+    {
+        [Test]
+        public void CreatingUsingStackalloc()
+        {
+            USpan<byte> data = stackalloc byte[8];
+            Assert.That(data.length, Is.EqualTo(8));
+            data[0] = 1;
+            data[1] = 2;
+
+            Assert.That(data[0], Is.EqualTo(1));
+            Assert.That(data[1], Is.EqualTo(2));
+            Assert.That(Allocations.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AccessingSpanOutOfRange()
+        {
+            USpan<byte> data = stackalloc byte[8];
+            try
+            {
+                data[8] = 0;
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void Slicing()
+        {
+            USpan<byte> data = stackalloc byte[8];
+            Span<byte> referenceData = stackalloc byte[8];
+            for (uint i = 0; i < 8; i++)
+            {
+                data[i] = (byte)i;
+                referenceData[(int)i] = (byte)i;
+            }
+
+            USpan<byte> slice = data[2..4];
+            Span<byte> referenceSlice = referenceData[2..4];
+
+            Assert.That(slice.length, Is.EqualTo(referenceSlice.Length));
+            Assert.That(slice.ToArray(), Is.EqualTo(referenceSlice.ToArray()));
+
+            using RandomGenerator rng = new();
+            for (int i = 0; i < 32; i++)
+            {
+                uint length = rng.NextUInt(8, 16);
+                uint sliceLength = rng.NextUInt(1, 4);
+                uint sliceStart = rng.NextUInt(0, length - sliceLength);
+                TestRandomSlice(length, sliceStart, sliceLength);
+            }
+
+            static void TestRandomSlice(uint length, uint sliceStart, uint sliceLength)
+            {
+                USpan<byte> data = stackalloc byte[(int)length];
+                Span<byte> referenceData = stackalloc byte[(int)length];
+                for (uint i = 0; i < length; i++)
+                {
+                    data[i] = (byte)i;
+                    referenceData[(int)i] = (byte)i;
+                }
+
+                Range sliceRange = (int)sliceStart..(int)(sliceStart + sliceLength);
+                USpan<byte> slice = data[sliceRange];
+                Span<byte> referenceSlice = referenceData[sliceRange];
+                Assert.That(slice.length, Is.EqualTo(referenceSlice.Length));
+                Assert.That(slice.ToArray(), Is.EqualTo(referenceSlice.ToArray()));
+            }
+        }
+
+        [Test]
+        public void SliceOutOfRange()
+        {
+            Span<byte> data = stackalloc byte[8];
+            try
+            {
+                Span<byte> slice = data[8..9];
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+            USpan<byte> uData = stackalloc byte[8];
+            try
+            {
+                USpan<byte> slice = uData[8..9];
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
