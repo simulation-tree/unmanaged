88202913e38b92b31776dbae5ee5bfe4cc914f38
Phill <philldamaskin@gmail.com>
2024-11-15 09:30:38 -0500

    Create RuntimeTypeHandleTests.cs

---
+++ tests/RuntimeTypeHandleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Unmanaged.Tests
+{
+    public class RuntimeTypeHandleTests
+    {
+        [Test]
+        public void CastTypeAddressBackToHandle()
+        {
+            nint address = RuntimeTypeHandle.ToIntPtr(typeof(int).TypeHandle);
+            RuntimeTypeHandle handle = RuntimeTypeHandle.FromIntPtr(address);
+            Type? type = Type.GetTypeFromHandle(handle);
+            Assert.That(type, Is.EqualTo(typeof(int)));
+        }
+    }
+}
